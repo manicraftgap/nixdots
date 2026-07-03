53dfde9cf7779f81fe6087b67698b1e3ba6c63ae
manicraftgap <141034309+manicraftgap@users.noreply.github.com>
2026-07-03 21:32:59 +0000

    added ns to workign home.packages

--- home.nix
+++ home.nix
@@ -47,5 +47,14 @@ in
 
   home.packages = with pkgs; [
     yazi
+    pkgs.writeShellApplication 
+    {
+      name = "ns";
+      runtimeInputs = with pkgs; [
+        fzf
+        nix-search-tv
+      ];
+      text = builtins.readFile "${pkgs.nix-search-tv.src}/nixpkgs.sh";
+    }
   ];
 }
