a5f73e34623a99aecd022fc31eac1d3c23d1780e
manicraftgap <141034309+manicraftgap@users.noreply.github.com>
2026-07-03 21:07:10 +0000

    added nix search

--- home.nix
+++ home.nix
@@ -45,4 +45,19 @@ in
     })
     configs;
 
+home.packages = with pkgs; [
+  pkgs.writeShellApplication 
+  {
+    name = "ns";
+    runtimeInputs = with pkgs; [
+      fzf
+      nix-search-tv
+    ];
+    text = builtins.readFile "${pkgs.nix-search-tv.src}/nixpkgs.sh";
+}
+]
+
+
+
+
 }
