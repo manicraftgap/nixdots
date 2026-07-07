ddd67a1647a05cca9c9f73e3e8bf86f32b96fa5a
manicraftgap <manuelitoa1811@proton.me>
2026-07-06 21:04:06 -0500

    added hyprland modules and fixed autostart

--- config/hypr/autostart.lua
+++ config/hypr/autostart.lua
@@ -1,18 +1,19 @@
 -- ~/.config/hypr/autostart.lua
 
--- Application execution hooks
-hl.exec_cmd("uwsm-app -- elephant")
-hl.exec_cmd("uwsm-app -- walker --gapplication-service")
-hl.exec_cmd("uwsm-app -- waybar")
+hl.on("hyprland.start", function()
+  -- Application execution hooks
+  hl.exec_cmd("uwsm-app -- elephant")
+  hl.exec_cmd("uwsm-app -- walker --gapplication-service")
+  hl.exec_cmd("waybar")
 
--- Core Environment Optimization
-hl.exec_cmd("systemctl --user import-environment WAYLAND_DISPLAY XDG_CURRENT_DESKTOP")
-hl.exec_cmd("dbus-update-activation-environment --systemd WAYLAND_DISPLAY XDG_CURRENT_DESKTOP")
-hl.exec_cmd("/usr/lib/polkit-gnome/polkit-gnome-authentication-agent-1")
+  -- Core Environment Optimization
+  hl.exec_cmd("systemctl --user import-environment WAYLAND_DISPLAY XDG_CURRENT_DESKTOP")
+  hl.exec_cmd("dbus-update-activation-environment --systemd WAYLAND_DISPLAY XDG_CURRENT_DESKTOP")
+  hl.exec_cmd("/usr/lib/polkit-gnome/polkit-gnome-authentication-agent-1")
 
--- Background System Processes
-hl.exec_cmd("uwsm-app -- hypridle") -- Fixed!
-hl.exec_cmd("uwsm-app -- mako")
-hl.exec_cmd("uwsm-app -- fcitx5 --disable notificationitem")
-hl.exec_cmd("uwsm-app -- swaybg -i ~/adam.jpeg -m fill")
-hl.exec_cmd("uwsm-app -- hyprsunset")
+  -- Background System Processes
+  hl.exec_cmd("uwsm-app -- hypridle")
+  hl.exec_cmd("uwsm-app -- mako")
+  hl.exec_cmd("uwsm-app -- swaybg -i ~/Adam.jpeg -m fill")
+  hl.exec_cmd("uwsm-app -- hyprsunset")
+end)
