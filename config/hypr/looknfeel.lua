907b2d9fb2f23008b724c03f26dfaff87c0cd1a0
manicraftgap <manuelitoa1811@proton.me>
2026-07-13 19:58:28 -0500

    removed hyprland animations

--- config/hypr/looknfeel.lua
+++ config/hypr/looknfeel.lua
@@ -8,19 +8,6 @@ hl.config({
     rounding = 0
   },
   animations = {
-    enabled = true,
-    animation = {
-      { "windows",          0, 1, "default" },
-      { "windowsMove",      1, 1, "default" },
-      { "workspaces",       0, 1, "default" },
-      { "specialWorkspace", 0, 1, "default" },
-      { "layers",           0, 1, "default" },
-      { "fade",             1, 1, "default" },
-      { "border",           1, 1, "default" },
-      { "borderangle",      1, 1, "default" }
-    }
-  },
-  windowrulev2 = {
-    "noanim, workspace:name:special:scratchpad"
+    enabled = false
   }
 })
